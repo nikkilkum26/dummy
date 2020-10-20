6af05ecb6294e9a53cb46063266a4b2d8d82e7c6
nikkilkum26 <nikkilkumar26@gmail.com>
2020-10-20 12:21:56 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-// const bodyParser = require("body-parser");
-// const mongodb = require("mongodb");
-// const mongoClient = mongodb.MongoClient;
-// const cors = require('cors')
+// let bodyParser = require("body-parser");
+let mongodb = require("mongodb");
+let mongoClient = mongodb.MongoClient;
+let cors = require('cors')
 
-// const url = "mongodb+srv://pswd123:wI8aD6bxgryMg0eg@zencluster.y8kfe.mongodb.net/?retryWrites=true&w=majority";
+let url = "mongodb+srv://pswd123:wI8aD6bxgryMg0eg@zencluster.y8kfe.mongodb.net/?retryWrites=true&w=majority";
 
 // router.use(bodyParser.json());
-// router.use(cors({
-//   origin: "*"
-// }));
+router.use(cors({
+  origin: "*"
+}));
 
 
 /* GET home page. */
@@ -18,39 +18,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// router.get('/read', async function(req, res, next) {
-//   try{
-//     let client = await mongoClient.connect(url);
+router.get('/read', async function(req, res, next) {
+  try{
+    let client = await mongoClient.connect(url);
 
-//   let db = client.db("zen");
-//   let list = await db.collection("userform").find().toArray();
-//   client.close();
-//   res.json(list)
-// }
-// catch(err){
-//   console.log(err);
-// }
-
-
-
-// })
-
-// router.post("/insert", async function (req, res) {
-//   try {let client = await mongoClient.connect(url);
-//   let db = client.db("zen");
-//   let insertedList = await db.collection("userform").insertOne(req.body )
-//   client.close();
-//   res.json({
-//     message: "created",
-//     id: insertedList.insertedId
-
-//   })}
-//   catch(err){
-//     console.log(err);
-//   }
+  let db = client.db("zen");
+  let list = await db.collection("userform").find().toArray();
+  client.close();
+  res.json(list)
+}
+catch(err){
+  console.log(err);
+}
 
 
-// })
+
+})
+
+router.post("/insert", async function (req, res) {
+  try {let client = await mongoClient.connect(url);
+  let db = client.db("zen");
+  let insertedList = await db.collection("userform").insertOne(req.body )
+  client.close();
+  res.json({
+    message: "created",
+    id: insertedList.insertedId
+
+  })}
+  catch(err){
+    console.log(err);
+  }
+
+
+})
 
 
 
