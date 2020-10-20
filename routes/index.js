4da36cc844c5adaadfd80e9c6ec96dc1025bff43
nikkilkum26 <nikkilkumar26@gmail.com>
2020-10-20 12:13:34 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -18,39 +18,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/read', async function(req, res, next) {
-  try{
-    let client = await mongoClient.connect(url);
+// router.get('/read', async function(req, res, next) {
+//   try{
+//     let client = await mongoClient.connect(url);
 
-  let db = client.db("zen");
-  let list = await db.collection("userform").find().toArray();
-  client.close();
-  res.json(list)
-}
-catch(err){
-  console.log(err);
-}
-
-
-
-})
-
-router.post("/insert", async function (req, res) {
-  try {let client = await mongoClient.connect(url);
-  let db = client.db("zen");
-  let insertedList = await db.collection("userform").insertOne(req.body )
-  client.close();
-  res.json({
-    message: "created",
-    id: insertedList.insertedId
-
-  })}
-  catch(err){
-    console.log(err);
-  }
+//   let db = client.db("zen");
+//   let list = await db.collection("userform").find().toArray();
+//   client.close();
+//   res.json(list)
+// }
+// catch(err){
+//   console.log(err);
+// }
 
 
-})
+
+// })
+
+// router.post("/insert", async function (req, res) {
+//   try {let client = await mongoClient.connect(url);
+//   let db = client.db("zen");
+//   let insertedList = await db.collection("userform").insertOne(req.body )
+//   client.close();
+//   res.json({
+//     message: "created",
+//     id: insertedList.insertedId
+
+//   })}
+//   catch(err){
+//     console.log(err);
+//   }
+
+
+// })
 
 
 
