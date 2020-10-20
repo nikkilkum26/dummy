c71888cdaee02f49e46964c4c1907b5186526b89
nikkilkum26 <nikkilkumar26@gmail.com>
2020-10-20 13:15:39 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,24 @@ var express = require('express');
 var router = express.Router();
 let mongodb = require("mongodb");
 let mongoClient=mongodb.MongoClient;
-
-
+const bodyParser = require("body-parser");
+router.use(bodyParser.json());
+const cors = require("cors");
 
 let url = "mongodb+srv://pswd123:wI8aD6bxgryMg0eg@zencluster.y8kfe.mongodb.net/?retryWrites=true&w=majority";
 
+router.use(cors({
+  origin : "*"
+}))
 
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.send("Im working")
 });
 
-router.get('/read', async function(req, res, next) {
+router.get('/read', async function(req, res) {
   try{
     let client = await mongoClient.connect(url);
 
