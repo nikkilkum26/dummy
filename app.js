95f3a1c4dd8c6c3ae78f3bdab0f51fed784599aa
nikkilkum26 <nikkilkumar26@gmail.com>
2020-10-20 12:02:21 +0530

    update

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+app.use(cors({
+  origin:"*"
+}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
